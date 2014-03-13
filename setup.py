f1efdde6e58d01e64effb310cdfdbea91d272854
Ellison Leao <ellisonleao@gmail.com>
2014-03-13 00:32:37 -0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyshorteners',
-    version='0.2.9',
+    version='0.2.10',
     license='MIT',
     description=('A simple URL shortening Python Lib, implementing '
                  'the most famous shorteners.'),
