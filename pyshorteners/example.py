72b021ede1c9ae8d63f55b27d91d7c668abfb3eb
Ellison Leão <ellisonleao@gmail.com>
2015-07-25 14:06:18 -0300

    fix example

--- pyshorteners/example.py
+++ pyshorteners/example.py
@@ -1,15 +1,17 @@
 # coding: utf-8
+from __future__ import print_function
+
 from pyshorteners.shorteners import Shortener
 
 
 def hello():
     short = Shortener('TinyurlShortener')
-    print """
+    print("""
 Hello World! Testing TinyurlShortener with www.google.com URL
 Shorten url: {}
 Expanded: {}
     """.format(short.short('http://www.google.com'),
-               short.expand('http://goo.gl/fbsS')),
+               short.expand('http://goo.gl/fbsS')))
 
 if __name__ == '__main__':
     hello()
