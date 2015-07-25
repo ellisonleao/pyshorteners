27c07b7c8647553ffee007d1de78792956e87d86
Ellison Leão <ellisonleao@gmail.com>
2015-07-25 14:10:40 -0300

    remove old runtests

--- runtests.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-from nose.core import run
-
-if __name__ == '__main__':
-    args = ['', '--cover-package=pyshorteners', '--with-coverage']
-    run(argv=args)
