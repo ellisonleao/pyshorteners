1d24296baac93a88b6f414a16ec5e10467ee810d
Ellison Leão <ellisonleao@gmail.com>
2015-07-27 15:25:59 -0300

    adding initial aw.sm shortener

---
+++ pyshorteners/shorteners/awsm.py
@@ -0,0 +1,46 @@
+# encoding: utf-8
+"""
+Aw.sm Shortener Implementation
+Needs a API_KEY
+Optional Params
+`tool` - String
+`channel` - 'twitter' 'facebook'. 'twitter' default value
+"""
+import requests
+
+from ..exceptions import ShorteningErrorException
+from .base import BaseShortener
+
+
+class AwsmShortener(BaseShortener):
+    api_url = 'http://api.awe.sm/'
+
+    def __init__(self, **kwargs):
+        if not kwargs.get('api_key', False):
+            raise TypeError('api_key missing from kwargs')
+        self.api_key = kwargs.get('api_key')
+        self.tool = kwargs.get('tool', AwsmShortener._generate_random_tool())
+        self.channel = kwargs.get('channel', 'twitter')
+
+    @staticmethod
+    def _generate_random_tool():
+        import string
+        import random
+        return ''.join(random.choice(string.letters + string.digits)
+                       for _ in range(4))
+
+    def short(self, url):
+        params = {
+            'v': 3,
+            'url': url,
+            'key': self.api_key,
+            'tool': self.tool,
+            'channel': self.channel
+        }
+        url = '{0}url.txt'.format(self.api_url)
+        response = requests.post(url, params=params)
+        if response.ok:
+            return response.text
+        raise ShorteningErrorException('There was an error shortening '
+                                       'this url - {0}'.format(
+                                           response.content))
---
+++ tests/test_awsm.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# encoding: utf-8
+try:
+    from urllib import urlencode
+except ImportError:
+    from urllib.parse import urlencode
+
+from pyshorteners import Shortener
+from pyshorteners.shorteners import AwsmShortener
+from pyshorteners.exceptions import (ShorteningErrorException,
+                                     ExpandingErrorException)
+
+import responses
+import pytest
+
+api_key = 'FAKE_KEY'
+s = Shortener('AwsmShortener', api_key=api_key, tool='abcde')
+short_url = 'http://aw.sm/rjf0oI'
+expanded = 'http://www.test.com'
+
+
+@responses.activate
+def test_awsm_short_method():
+    # mock response
+    params = urlencode({
+        'url': expanded,
+        'key': api_key,
+        'channel': 'twitter',
+        'tool': 'abcde',
+        'v': 3
+    })
+    url = '{0}url.txt?{1}'.format(s.api_url, params)
+    responses.add(responses.POST, url, body=short_url, match_querystring=True)
+
+    shorten = s.short(expanded)
+    assert shorten == short_url
+
+
+@responses.activate
+def test_awsm_short_method_bad_response():
+    url = '{}url.txt'.format(s.api_url)
+    responses.add(responses.POST, url, body=short_url, status=400)
+
+    with pytest.raises(ShorteningErrorException):
+        s.short(expanded)
+
+
+@responses.activate
+def test_googl_expand_method_bad_response():
+    responses.add(responses.GET, short_url, body='', status=400,
+                  match_querystring=True)
+
+    with pytest.raises(ExpandingErrorException):
+        s.expand(short_url)
+
+
+def test_generate_tool_staticmethod():
+    tool = AwsmShortener._generate_random_tool()
+    assert len(tool) == 4
+
+
+def test_bad_key():
+    s = Shortener('AwsmShortener')
+
+    with pytest.raises(TypeError):
+        s.short(expanded)
