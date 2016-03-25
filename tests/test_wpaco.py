c79424321b31e0e8b009cae7d6ffdad28d741ff4
Ellison Leão <ellisonleao@gmail.com>
2016-03-24 21:06:16 -0300

    adding wp-a.co shortener #40

---
+++ tests/test_wpaco.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# encoding: utf-8
+try:
+    from urllib import urlencode
+except ImportError:
+    from urllib.parse import urlencode
+
+
+from pyshorteners import Shortener, Shorteners
+from pyshorteners.exceptions import ShorteningErrorException
+
+import responses
+import pytest
+
+s = Shortener(Shorteners.WPACO)
+shorten = 'http://wp-a.co/test'
+expanded = 'http://www.test.com'
+
+
+@responses.activate
+def test_wpaco_short_method():
+    # mock responses
+    params = urlencode({
+        'url': expanded,
+        'method': 'http',
+        'customshort': ''
+    })
+    mock_url = '{}?{}'.format(s.api_url, params)
+    responses.add(responses.GET, mock_url, body=shorten,
+                  match_querystring=True)
+
+    shorten_result = s.short(expanded)
+
+    assert shorten_result == shorten
+    assert s.shorten == shorten_result
+    assert s.expanded == expanded
+
+
+@responses.activate
+def test_wpaco_short_method_bad_response():
+    # mock responses
+    params = urlencode({
+        'url': expanded,
+        'method': 'http',
+        'customshort': ''
+    })
+    mock_url = '{}?{}'.format(s.api_url, params)
+    responses.add(responses.GET, mock_url, body=shorten, status=400,
+                  match_querystring=True)
+
+    with pytest.raises(ShorteningErrorException):
+        s.short(expanded)
