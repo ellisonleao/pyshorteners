44c01550116d272e6c2808b026169a566f6ada4d
Andrés Reyes Monge <armonge@gmail.com>
2019-10-15 10:46:19 +0200

    Adding a proxy test

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,62 +1,84 @@
+# pylint: disable=W0212,C0116,C0114
+import threading
+
 from pyshorteners.base import BaseShortener
 from pyshorteners.exceptions import BadURLException, ExpandingErrorException
 
 import pytest
-import responses
+import proxy
+
+
+@pytest.fixture
+def proxy_url():
+    thread = threading.Thread(
+        target=proxy.main, args=(["--hostname", "127.0.0.1", "--port", "8899"],)
+    )
+    thread.daemon = True
+    thread.start()
+
+    yield f"http://127.0.0.1:8899"
 
 
 def test_base_init_params_become_properties():
-    b = BaseShortener(a=1, b=2)
-    assert b.a == 1
-    assert b.b == 2
+    # pylint: disable=E1101
+    shortener = BaseShortener(a=1, b=2)
+    assert shortener.a == 1
+    assert shortener.b == 2
     # check default params
-    assert b.timeout == 2
-    assert b.verify is True
+    assert shortener.timeout == 2
+    assert shortener.verify is True
 
 
 def test_base_clean_url_method():
     # good
-    url = 'www.google.com'
-    assert BaseShortener.clean_url(url) == f'http://{url}'
+    url = "www.google.com"
+    assert BaseShortener.clean_url(url) == f"http://{url}"
 
     # bad
     with pytest.raises(BadURLException):
-        BaseShortener.clean_url('http://')
+        BaseShortener.clean_url("http://")
 
 
 def test_base_expand_method():
-    b = BaseShortener()
-    url = 'http://httpbin.org/get'
-    assert b.expand(url) == url
+    shortener = BaseShortener()
+    url = "http://httpbin.org/get"
+    assert shortener.expand(url) == url
 
 
 def test_base_expand_method_bad_response():
-    b = BaseShortener()
+    shortener = BaseShortener()
     with pytest.raises(ExpandingErrorException):
-        b.expand('http://httpbin.org/status/400')
+        shortener.expand("http://httpbin.org/status/400")
 
 
 def test_base_get_request_bad_url():
-    b = BaseShortener()
-    url = '.....'
+    shortener = BaseShortener()
+    url = "....."
     with pytest.raises(BadURLException):
-        b._get(url)
+        shortener._get(url)
 
 
 def test_base_post_request():
-    b = BaseShortener()
-    url = 'http://httpbin.org/status/200'
-    assert b._post(url).status_code == 200
+    shortener = BaseShortener()
+    url = "http://httpbin.org/status/200"
+    assert shortener._post(url).status_code == 200
 
 
 def test_base_post_request_bad_url():
-    b = BaseShortener()
-    url = '.....'
+    shortener = BaseShortener()
+    url = "....."
     with pytest.raises(BadURLException):
-        b._post(url)
+        shortener._post(url)
 
 
 def test_base_short_method_raises_notimplemented():
-    b = BaseShortener()
+    shortener = BaseShortener()
     with pytest.raises(NotImplementedError):
-        b.short('http://someurl')
+        shortener.short("http://someurl")
+
+
+def test_base_proxy(proxy_url):
+    # pylint: disable=W0621
+    shortener = BaseShortener(proxies={"http": proxy_url})
+    url = "http://httpbin.org/status/200"
+    assert shortener._post(url).status_code == 200
