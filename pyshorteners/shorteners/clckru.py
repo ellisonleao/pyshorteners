75f66dede7ad5ee07c922ee71fe3a247c624de00
Ellison Leão <ellisonleao@gmail.com>
2020-02-27 11:39:30 -0300

    adding clck.ru docs

--- pyshorteners/shorteners/clckru.py
+++ pyshorteners/shorteners/clckru.py
@@ -5,12 +5,34 @@ from ..exceptions import ShorteningErrorException
 class Shortener(BaseShortener):
     """
     Clck.ru shortener implementation
-    No config params needed
+
+    Example:
+
+        >>> import pyshorteners
+        >>> s = pyshorteners.Shortener()
+        >>> s.clckru.short('http://www.google.com')
+        'http://clck.ru/TEST'
+        >>> s.clckru.expand('http://clck.ru/TEST')
+        'http://www.google.com'
+
     """
-    api_url = 'https://clck.ru/--'
+
+    api_url = "https://clck.ru/--"
 
     def short(self, url):
-        response = self._get(self.api_url, params={'url': url})
+        """Short implementation for Clck.ru
+
+        Args:
+            url: the URL you want to shorten
+
+        Returns:
+            A string containing the shortened URL
+
+        Raises:
+            ShorteningErrorException: If the API returns an error as response
+        """
+
+        response = self._get(self.api_url, params={"url": url})
         if response.ok:
             return response.text.strip()
         raise ShorteningErrorException(response.content)
