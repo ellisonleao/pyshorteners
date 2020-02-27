3b8df7940b98cea83a91ef3975223cde3c2f6b60
Ellison Leão <ellisonleao@gmail.com>
2020-02-27 11:39:02 -0300

    adding chilp.it docs

--- pyshorteners/shorteners/chilpit.py
+++ pyshorteners/shorteners/chilpit.py
@@ -5,12 +5,32 @@ from ..exceptions import ShorteningErrorException
 class Shortener(BaseShortener):
     """
     Chilp.it shortener implementation
-    No config params needed
+
+    Example:
+
+        >>> import pyshorteners
+        >>> s = pyshorteners.Shortener()
+        >>> s.chilpit.short('http://www.google.com')
+        'http://chilp.it/TEST'
+        >>> s.chilpit.expand('http://chilp.it/TEST')
+        'http://www.google.com'
     """
-    api_url = 'http://chilp.it/api.php'
+
+    api_url = "http://chilp.it/api.php"
 
     def short(self, url):
-        response = self._get(self.api_url, params={'url': url})
+        """Short implementation for Chilp.it
+
+        Args:
+            url: the URL you want to shorten
+
+        Returns:
+            A string containing the shortened URL
+
+        Raises:
+            ShorteningErrorException: If the API returns an error as response
+        """
+        response = self._get(self.api_url, params={"url": url})
         if response.ok:
             return response.text.strip()
         raise ShorteningErrorException(response.content)
