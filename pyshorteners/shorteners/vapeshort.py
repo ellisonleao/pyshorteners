2430f7742afc7aa7507362b89a71c5e4b1ad15b3
vapeluckymanbot <104741743+vapeluckymanbot@users.noreply.github.com>
2022-05-22 13:00:45 +0530

    Update vapeshort.py

--- pyshorteners/shorteners/vapeshort.py
+++ pyshorteners/shorteners/vapeshort.py
@@ -1,113 +1,16 @@
-import json
-
-from ..exceptions import (
-    ShorteningErrorException,
-    BadAPIResponseException,
-    ExpandingErrorException,
-)
-from ..base import BaseShortener
-
+from pyshorteners.base import BaseShortener
 
 class Shortener(BaseShortener):
-    """short.vapeluckyman.xyz implementation.
-    Args:
-        api_key (str): short.vapeluckyman.xyz API key.
-        user_id (str): short.vapeluckyman.xyz user id.
-        domain (str, optional): Domain used upon shortening, options are:
-            - ``ad.fly`` (default)
-            - ``q.gs``
-            - ``custom.com``
-            - ``0`` (Random domain)
-        type (int, optional): For advertising on the shortened link, options
-            are:
-            - ``1``, ``int``, ``interstitial`` - Interstitial advertising
-            - ``2`` - No advertising
-            - ``3``, ``banner`` - Framed Banner
-        group_id (int, optional): API parameter `group_id`.
-    Example:
-        >>> import pyshorteners
-        >>> s = pyshorteners.Shortener(api_key='YOUR_KEY', user_id='USER_ID',
-                                       domain='test.us', group_id=12, type='int')
-        >>> s.adfly.short('http://www.google.com')
-        'http://test.us/TEST'
     """
-
-    api_url = "http://https://short.vapeluckyman.xyz/api"
+    Docstring
+    """
+    api_url = 'https://short.vapeluckyman.xyz/api'
 
     def short(self, url):
-        """Short implementation for short.vapeluckyman.xyz.
-        Args:
-            url (str): The URL you want to shorten.
-        Returns:
-            str: The shortened URL.
-        Raises:
-            BadAPIResponseException: If the data is malformed or we got a bad
-                status code on API response.
-            ShorteningErrorException: If the API Returns an error as response.
-        """
-        url = self.clean_url(url)
-        shorten_url = f"{self.api_url}/shorten"
-        payload = {
-            "domain": getattr(self, "domain", "short.vapeluckyman.xyz"),
-            "advert_type": getattr(self, "type", "int"),
-            "group_id": getattr(self, "group_id", None),
-            "key": self.api_key,
-            "user_id": self.user_id,
-            "url": url,
-        }
-        response = self._post(shorten_url, data=payload)
-        if not response.ok:
-            raise BadAPIResponseException(response.content)
-
-        try:
-            data = response.json()
-        except json.decoder.JSONDecodeError:
-            raise BadAPIResponseException("API response could not be decoded")
-
-        if data.get("errors"):
-            errors = ",".join(i["msg"] for i in data["errors"])
-            raise ShorteningErrorException(errors)
-
-        if not data.get("data"):
-            raise BadAPIResponseException(response.content)
-
-        return data["data"][0]["short_url"]
+        pass
 
     def expand(self, url):
-        """Expand implementation for short.vapeluckyman.xyz.
-        Args:
-            url (str): The URL you want to expand.
-        Returns:
-            str: The expanded URL.
-        Raises:
-            BadAPIResponseException: If the data is malformed or we got a bad
-                status code on API response.
-            ExpandingErrorException: If the API Returns an error as response.
-        """
-        url = self.clean_url(url)
-        expand_url = f"{self.api_url}/expand"
-        payload = {
-            "domain": getattr(self, "domain", "short.vapeluckyman.xyz"),
-            "advert_type": getattr(self, "type", "int"),
-            "group_id": getattr(self, "group_id", None),
-            "key": self.api_key,
-            "user_id": self.user_id,
-            "url": url,
-        }
-        response = self._post(expand_url, data=payload)
-        if not response.ok:
-            raise BadAPIResponseException(response.content)
+        pass
 
-        try:
-            data = response.json()
-        except json.decoder.JSONDecodeError:
-            raise BadAPIResponseException("API response could not be decoded")
-
-        if data.get("errors"):
-            errors = ",".join(i["msg"] for i in data["errors"])
-            raise ExpandingErrorException(errors)
-
-        if not data.get("data"):
-            raise BadAPIResponseException(response.content)
-
-        return data["data"][0]["url"]
+    def custom_method(self):
+        pass
